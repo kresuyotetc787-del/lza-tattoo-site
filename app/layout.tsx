511eeb9b67593cadb57eba066cf0561b2e5c7b89
Николай <k2727@MacBook-Air-Nikolaj.local>
2026-04-17 14:24:55 +0300

    fix layout and add google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ const playfair = Playfair_Display({
   weight: ["400", "700"],
 });
 
-
 export const metadata: Metadata = {
   title: "ŁZA TATTOO & PIERCING | Tatuaże i piercing",
   description: "Profesjonalne studio tatuażu i piercingu w Lublinie.",
@@ -16,5 +15,16 @@ export const metadata: Metadata = {
   },
 };
 
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pl" className={`${playfair.className} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
 
 
