57ad1fc6566386203a0d9d9b3c89c491703984ad
Николай <k2727@MacBook-Air-Nikolaj.local>
2026-04-17 14:13:53 +0300

    add google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,25 +7,14 @@ const playfair = Playfair_Display({
   weight: ["400", "700"],
 });
 
+
 export const metadata: Metadata = {
   title: "ŁZA TATTOO & PIERCING | Tatuaże i piercing",
-  description:
-    "Profesjonalne studio tatuażu i piercingu w Lublinie. Tatuaże, piercing, rezerwacje przez Instagram.",
+  description: "Profesjonalne studio tatuażu i piercingu w Lublinie.",
+  verification: {
+    google: "2lfduOL1KPtVoA9w9qJgf7JT2vE08Iz2nj",
+  },
 };
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="pl" className={`${playfair.className} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
-}
-
-
 
 
 
